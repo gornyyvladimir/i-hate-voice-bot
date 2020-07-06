84f06d25a60b407e8f79722431e4ac61bad8afd9
Vladimir Gornyj <vladimir.gornyj@piano.io>
2020-07-06 13:30:57 +0300

    add bot username

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 3000;
 const url = process.env.URL || 'https://i-hate-voice-bot.herokuapp.com';
 
 const bot = new Telegraf(token);
+// for bot working in chats
+bot.telegram.getMe().then(botInfo => {
+  bot.options.username = botInfo.username;
+});
 bot.start(ctx => ctx.reply('Привет! Я посылаю оскорбления в ответ на голосовые сообщения.'));
 bot.help(ctx => ctx.reply('Пришли мне голосовое сообщения, а я отвечу чем нибудь мерзким.'));
 bot.on(['voice', 'video_note'], ctx => ctx.reply(getMessage(messages)));
